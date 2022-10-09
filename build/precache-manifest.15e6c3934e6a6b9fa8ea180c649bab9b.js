f6d1e541148ca91b5f274185b3fac09540fa704f
Nisarg Patel <nishupatel0711@outlook.com>
2022-10-09 07:25:08 -0400

    React project code added

---
+++ build/precache-manifest.15e6c3934e6a6b9fa8ea180c649bab9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a166263cac101d9c0b70",
+    "url": "/NisargPatel0711/comp3104/static/css/main.94b23126.chunk.css"
+  },
+  {
+    "revision": "a166263cac101d9c0b70",
+    "url": "/NisargPatel0711/comp3104/static/js/main.a166263c.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/NisargPatel0711/comp3104/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "b7c10b2bbc990fb90fd4",
+    "url": "/NisargPatel0711/comp3104/static/js/runtime~main.b7c10b2b.js"
+  },
+  {
+    "revision": "c684e95486feb866fe4b2e7444baff98",
+    "url": "/NisargPatel0711/comp3104/index.html"
+  }
+];
